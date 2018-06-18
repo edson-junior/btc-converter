413bb96291fdaed7978076be3813d110e6a7e320
Edson Junior <soneddjr@gmail.com>
2018-06-18 18:00:58 +0200

    Conhecendo o Commander

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,17 @@
-import { expect } from 'chai';
+const { expect } = require('chai');
+const { exec } = require('child_process');
+const btcConverter = './src/main.js';
+const pkg = require('../package.json');
 
-describe('Main', () => {
+describe('Main CLI', () => {
+  it('should return version of btc-converter', (done) => {
+    exec(`${btcConverter} --version`, (err, stdout, stderr) => {
+      if (err) {
+        throw err;
+      }
 
+      expect(stdout.replace('\n', '')).to.be.equal(pkg.version);
+      done();
+    });
+  });
 });
