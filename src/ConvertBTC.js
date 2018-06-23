a14d12fe65dda1fc89f27131a527a609c2abf004
Edson Junior <soneddjr@gmail.com>
2018-06-23 20:12:13 +0100

    Utilizando a API para obter dados reais

--- src/ConvertBTC.js
+++ src/ConvertBTC.js
@@ -1,5 +1,12 @@
+const request = require('request');
+
 function convertBTC(currency = 'USD', amount = 1) {
-  return `${amount} BTC to ${currency} = 2000.00`;
+  const url = `https://apiv2.bitcoinaverage.com/convert/global?from=BTC&to=${currency}&amount=${amount}`;
+
+  request(url, (error, response, body) => {
+    const apiResponse = JSON.parse(body);
+    console.log(`1 BTC to USD = ${apiResponse.price}`); // eslint-disable-line
+  });
 }
 
 module.exports = convertBTC;
--- tests/ConvertBTC.spec.js
+++ tests/ConvertBTC.spec.js
@@ -1,13 +1,41 @@
+const nock = require('nock');
 const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
 const expect = chai.expect;
+
+chai.use(sinonChai);
+
 const convertBTC = require('../src/ConvertBTC');
 
 describe('ConvertBTC', () => {
-  it('should return USD as currency and 1 as default amount', () => {
-    expect(convertBTC()).to.be.equal('1 BTC to USD = 2000.00');
+  let consoleStub;
+
+  const responseMock = {
+    "time": "2018-06-23 18:44:12",
+    "success": true,
+    "price": 6142.77
+  }
+
+  beforeEach(() => {
+    consoleStub = sinon.stub(console, 'log');
   });
 
-  it('should return BRL as currency and 10 as amount when defined', () => {
-    expect(convertBTC('BRL', 10)).to.be.equal('10 BTC to BRL = 2000.00');
+  afterEach(() => {
+    console.log.restore();
+  });
+
+  it('should use currency USD and 1 as default amount', () => {
+    nock('https://apiv2.bitcoinaverage.com')
+      .get('/convert/global')
+      .query({ from: 'BTC', to: 'USD', amount: 1 })
+      .reply(200, responseMock);
+
+    convertBTC('USD', 10);
+
+    setTimeout(() => {
+      expect(consoleStub).to.have.been.calledWith('1 BTC to USD = 6142.77');
+      done();
+    }, 300);
   });
 });
